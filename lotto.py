8155236421c38d3d42941ee1898c84a2233a4d2c
mini98 <44043938+minnie98@users.noreply.github.com>
2020-07-06 21:37:59 +0900

    Update lotto.py

--- lotto.py
+++ lotto.py
@@ -1,3 +1,9 @@
+#############################
+### Backjoon Algorithm    ###    
+### No.6603               ###
+#############################
+
+
 from itertools import combinations
 
 while True:
@@ -8,6 +14,3 @@ while True:
     for case in combinations(line[1:], 6):
         print(' '.join(map(str, case)))
     print('')
-
-#input().split() //값 여러개 받기 //결과는 문자열 리스트
-#map //형 변환 (정수로, 실수로 , ... )
