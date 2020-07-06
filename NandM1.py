05ad8e8862560f1b945c07ae755f32b405f6ac81
mini98 <44043938+minnie98@users.noreply.github.com>
2020-07-06 21:37:23 +0900

    Update NandM1.py

--- NandM1.py
+++ NandM1.py
@@ -1,3 +1,9 @@
+#############################
+### Backjoon Algorithm    ###    
+### No.15649              ###
+#############################
+
+
 # N, M = map(int, input().split())
 # visited = [False for i in range(N)]
 # out = []
